20c71d5adc4858436ef5c2742dd31685fe391605
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2024-09-03 12:51:44 +0500

    Initial commit

--- main.js
+++ main.js
@@ -3,19 +3,25 @@ let form = document.querySelector(".form");
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
 let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
 let box = document.querySelector(".box-text");
 
+form.appendChild(box);
 
+btn3.addEventListener("click", () => {
+    let paragraphs = box.children;
+    if (paragraphs.length > 0) {
+        paragraphs[0].textContent = input.value;
+    }
+})
 
-
-form.addEventListener  ("submit", (e) => {
+form.addEventListener("submit", (e) => {
     let p = document.createElement("p");
     e.preventDefault();
 
     let random = Math.ceil(Math.random() * 1000);
 
     p.innerText = input.value;
-    form.append(box) 
     box.appendChild(p);
     input.value = "";
 
@@ -23,7 +29,6 @@ form.addEventListener  ("submit", (e) => {
         p.remove();
     })
 
-    p.style.fontSize = "20px";
-    p.style.background = `#${random}`;
-    
+    p.style.fontSize = "30px";
+    p.style.color = `#${random}`;
 })
